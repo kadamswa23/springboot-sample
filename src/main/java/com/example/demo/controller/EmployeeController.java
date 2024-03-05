9019c23a69615ff6d06d5388cf53bdc845ccba0d
kadamswa23 <kadamswa23@gmail.com>
2024-03-05 11:46:56 +0530

    add employee rest api

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -2,15 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
-public class HelloController {
+@RequestMapping("/employee")
+public class EmployeeController {
 
     @GetMapping
-    public String message() {
-        return"--------- Hello World ------------";
+    public String getEmployee() {
+        return"--------- add Employee ------------";
     }
 }
