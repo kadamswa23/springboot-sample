f0f999e41d34167800822d5eb23356b2765f3f7c
kadamswa23 <kadamswa23@gmail.com>
2024-03-05 12:42:05 +0530

    second commit

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,15 +1,21 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
 
+    @PostMapping
+    public ResponseEntity addEmployee(){
+        System.out.println("--------- add employee ------------");
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
     @GetMapping
     public String getEmployee() {
-        return"--------- add Employee ------------";
+        return"--------- get Employee ------------";
     }
 }
